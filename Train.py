6113f3fdb3cc104e4755ec0bc6cdfa1912c27f91
AustinTapp <atapp001@odu.edu>
2022-09-06 11:00:59 -0400

    Update to fix failure related to wandb logger

--- Train.py
+++ Train.py
@@ -13,7 +13,9 @@ if __name__ == "__main__":
     wandb_logger = WandbLogger(project="SSL4N")
 
     lr_monitor = LearningRateMonitor(logging_interval='epoch')
-    checkpoint_callback = ModelCheckpoint(dirpath="./saved_models/loss/", save_top_k=1, monitor="val_loss")
+    checkpoint_callback = ModelCheckpoint(dirpath="./saved_models/loss/", save_top_k=1, monitor="val_loss", save_on_train_epoch_end = True)
+    last_chpt = "./saved_models/loss/" + "epoch=93-step=150.ckpt"
+
     trainer = Trainer(
         logger=wandb_logger,
         accelerator="gpu",
@@ -26,6 +28,7 @@ if __name__ == "__main__":
     dims = 1
     trainer.fit(
         model=ViTATrain(),
+        ckpt_path=last_chpt,
         datamodule=MRIdata(
             batch_size=1,
             img_size=size,
