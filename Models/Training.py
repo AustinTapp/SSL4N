6fafc58c5b6740a5981de20e1552423ab53ee61d
Austin Tapp <atapp001@odu.edu>
2022-09-07 11:32:20 -0400

    fix wandb visualization

--- Models/Training.py
+++ Models/Training.py
@@ -4,6 +4,7 @@ from torch.nn import L1Loss
 from monai.losses import ContrastiveLoss
 
 from monai.visualize.img2tensorboard import plot_2d_or_3d_image
+import numpy as np
 from pytorch_lightning import LightningModule
 import torch
 
@@ -55,10 +56,6 @@ class ViTATrain(LightningModule):
         flat_out_v2 = outputs_v2.flatten(start_dim=1, end_dim=4)
 
         r_loss = self.L1(outputs_v1, gt_input)
-
-        if stage == 'train':
-            import pdb; pdb.set_trace()
-
         cl_loss = self.contrast(flat_out_v1, flat_out_v2)
 
         # Adjust the CL loss by Recon Loss
@@ -77,15 +74,17 @@ class ViTATrain(LightningModule):
                 'epoch': float(self.current_epoch),
                 'step': float(train_steps)}, batch_size=self.hparams.batch_size)
             self.logger.log_image(key="Ground Truth", images=[
-                gt_input.detach().cpu().numpy()[0, 0, :, :, 38]],
+                (gt_input.detach().cpu().numpy()*255)[0, 0, :, :, 38]],
                 caption=["GT"])
             self.logger.log_image(key="Input (Transformed) Images", images=[
-                inputs.detach().cpu().numpy()[0, 0, :, :, 38],
-                inputs_2.detach().cpu().numpy()[0, 0, :, :, 38]],
+                (inputs.detach().cpu().numpy()*255)[0, 0, :, :, 38],
+                (inputs_2.detach().cpu().numpy()*255)[0, 0, :, :, 38]],
                 caption=["Input 1", "Input 2"])
+            outputs_v1_array = np.clip(outputs_v1.detach().cpu().numpy(), 0, 1)
+            outputs_v2_array = np.clip(outputs_v2.detach().cpu().numpy(), 0, 1)
             self.logger.log_image(key="Reconstructed Images", images=[
-                outputs_v1.detach().cpu().numpy()[0, 0, :, :, 38],
-                outputs_v2.detach().cpu().numpy()[0, 0, :, :, 38]],
+                (outputs_v1_array*255)[0, 0, :, :, 38],
+                (outputs_v2_array*255)[0, 0, :, :, 38]],
                 caption=["Recon1", "Recon2"])
 
         return total_loss
--- Train.py
+++ Train.py
@@ -14,7 +14,6 @@ if __name__ == "__main__":
 
     lr_monitor = LearningRateMonitor(logging_interval='epoch')
     checkpoint_callback = ModelCheckpoint(dirpath="./saved_models/loss/", save_top_k=1, monitor="val_loss", save_on_train_epoch_end=True)
-    last_chpt = "./saved_models/loss/" + "epoch=93-step=150.ckpt"
 
     trainer = Trainer(
         logger=wandb_logger,
@@ -27,7 +26,7 @@ if __name__ == "__main__":
 
     trainer.fit(
         model=ViTATrain(),
-        ckpt_path=last_chpt,
+        #ckpt_path=last_chpt,
         datamodule=MRIdata(
             batch_size=1)
     )
