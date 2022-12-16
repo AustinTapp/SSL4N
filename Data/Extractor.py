5ffe373e3e91d0b84f45091f37a9af43e77ce55e
Austin Tapp <atapp001@odu.edu>
2022-12-15 20:16:33 -0500

    extractor for CQ500

--- Data/Extractor.py
+++ Data/Extractor.py
@@ -3,10 +3,10 @@ import shutil
 import SimpleITK as sitk
 
 if __name__ == '__main__':
-    data_dir = "D:\\Data\\SkullTest"
+    data_dir = "C:\\Users\\Austin Tapp\\Downloads\\CQ500"
     list_subfolders_with_paths = [f.path for f in os.scandir(data_dir) if f.is_dir()]
     for i in range(len(list_subfolders_with_paths)):
-        nifti_folder = os.path.join(list_subfolders_with_paths[i], "asNifti")
+        nifti_folder = os.path.join(data_dir, "asNifti")
         #print(nifti_folder)
         isExist = os.path.exists(nifti_folder)
         if not isExist:
@@ -14,29 +14,28 @@ if __name__ == '__main__':
         patient_subfolder_with_path = [f.path for f in os.scandir(list_subfolders_with_paths[i]) if f.is_dir()]
         for j in range(len(patient_subfolder_with_path)):
             #print(patient_subfolder_with_path[j].split('\\')[-1])
-            patient_imagefiles_with_path = [f.path for f in os.scandir(patient_subfolder_with_path[j]) if f.is_dir()]
-            for k in range(len(patient_imagefiles_with_path)):
-                z = 0
-                #print(patient_imagefiles_with_path[k])
-                if patient_imagefiles_with_path[k].endswith('Old'):
-                    shutil.rmtree(patient_imagefiles_with_path[k])
-                #elif patient_imagefiles_with_path[k].endswith('asNifti'):
-                #    shutil.rmtree(patient_imagefiles_with_path[k])
-                else:
-                    z = z+1
-                    DCMfiles_with_path = [f.path for f in os.scandir(patient_imagefiles_with_path[k]) if f.is_dir()]
-                    #print(DCMfiles_with_path[0])
+            patient_scanfiles_with_path = [f.path for f in os.scandir(patient_subfolder_with_path[j]) if f.is_dir()]
+            for k in range(len(patient_scanfiles_with_path)):
+                # print(patient_subfolder_with_path[j].split('\\')[-1])
+                patient_imagefiles_with_path = [f.path for f in os.scandir(patient_scanfiles_with_path[j]) if f.is_dir()]
+                for l in range(len(patient_imagefiles_with_path)):
+                    #print(patient_imagefiles_with_path[k])
+                    if patient_imagefiles_with_path[k].endswith('Old'):
+                        shutil.rmtree(patient_imagefiles_with_path[k])
+                    #elif patient_imagefiles_with_path[k].endswith('asNifti'):
+                    #    shutil.rmtree(patient_imagefiles_with_path[k])
+                    else:
+                        DCMfiles_path = [f.path for f in os.scandir(patient_imagefiles_with_path[k]) if f.is_dir()]
+                        #print(DCMfiles_path[0])
 
-                    reader = sitk.ImageSeriesReader()
-                    dicom_names = reader.GetGDCMSeriesFileNames(DCMfiles_with_path[0])
-                    reader.SetFileNames(dicom_names)
-                    dicom_image = reader.Execute()
-                    nifti_image_name = os.path.join(data_dir,
-                                       list_subfolders_with_paths[i].split('\\')[-1], "asNifti",
-                                       patient_subfolder_with_path[j].split('\\')[-1] + "_" +
-                                       patient_imagefiles_with_path[k].split('\\')[-1] + "_.nii.gz")
+                        reader = sitk.ImageSeriesReader()
+                        dicom_names = reader.GetGDCMSeriesFileNames(DCMfiles_path[0])
+                        reader.SetFileNames(dicom_names)
+                        dicom_image = reader.Execute()
+                        nifti_image_name = os.path.join(data_dir, "asNifti",
+                                           patient_subfolder_with_path[j].split('\\')[-1].split('-')[-1] + ".nii.gz")
 
-                    sitk.WriteImage(dicom_image, nifti_image_name)
-                    print(f"Writing {nifti_image_name} to file was successful...\n")
+                        sitk.WriteImage(dicom_image, nifti_image_name)
+                        print(f"Writing {nifti_image_name} to file was successful...\n")
 
     print("Done!")
